b23399e726b130aeb871bbb1b475160482b14f42
Vivek Patole <vivek@iknowlation.com>
2019-10-12 23:03:33 +0530

    Establish database connection with MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+let mongoose = require("mongoose");
 require('dotenv').config();
 
 // Create express app
@@ -7,6 +8,31 @@ let app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+
+// Database Host Name/IP
+const DB_HOST = process.env.DB_HOST;
+// Database Name
+const DB_NAME = process.env.DB_NAME;
+// Connection URL
+const DB_URL = 'mongodb://' + DB_HOST + '/' + DB_NAME;
+// Connection Options
+const DB_OPTIONS = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+};
+// Connect to the database
+mongoose.connect(DB_URL, DB_OPTIONS)
+    .then(function () {
+        console.log('Connected to MongoDB at %s', DB_URL);
+    })
+    .catch(function (error) {
+        console.log('Failed to connect Mongodb; Error: %s', error);
+        process.exit();
+    });
+
 
 app.listen(process.env.PORT);
 
