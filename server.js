2f467d24969975dd6cd6235a5741369c6d576f63
Vivek Patole <vivek@iknowlation.com>
2019-10-12 23:09:42 +0530

    Created middleware to check authorization token

--- server.js
+++ server.js
@@ -34,6 +34,34 @@ mongoose.connect(DB_URL, DB_OPTIONS)
     });
 
 
+// Middleware to check authorization token
+app.all("/*", checkToken, function (req, res, next) {
+    next();
+});
+
+// Function to check authorization token
+function checkToken(req, res, next) {
+    // Extract token from headers
+    let token = req.headers['authorization'];
+
+    if (token) {    // If token exists
+        if (token.startsWith('Bearer ')) {
+            // Remove Bearer from string
+            token = token.slice(7, token.length);
+        }
+
+        if (token == process.env.AUTH_TOKEN) {   // Valid token
+            next();
+        } else {    // Invalid token
+            res.status(401);
+            return res.json({ message: 'Invalid authorization token' });
+        }
+    } else {    // If token does not exists
+        res.status(403);
+        return res.json({ message: 'Authorization token is not supplied' });
+    }
+}
+
 app.listen(process.env.PORT);
 
 console.log('Express server started at http://localhost:%s', process.env.PORT);
